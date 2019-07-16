8caaaf6230068f04fe68c4bf0ddd00935bf426ef
prajat <rajat.singh1084@gmail.com>
2019-07-16 12:16:59 +0530

    project files added

---
+++ app.js
@@ -0,0 +1,16 @@
+module.exports = {
+  sayHello: function(){
+    return 'hello';
+  },
+  addNumbers: function(value1, value2){
+    return value1 + value2;
+  }
+}
+
+// export default sayHello=()=>{
+//   return 'hello';
+// }
+// export default addNumbers=(value1,value2)=>{
+//   return value1+value2;
+  
+// }
