1f283ce66c6e119de34fcc8dc68ded3787b0f81c
prajat <rajat.singh1084@gmail.com>
2019-07-17 11:07:27 +0530

    another add test included

--- app.js
+++ app.js
@@ -4,7 +4,19 @@ module.exports = {
   },
   addNumbers: function(value1, value2){
     return value1 + value2;
+  },
+  add: function(num1,num2){
+    if(Array.isArray(num1)){
+      let sum=0;
+      for(let i=0;i<num1.length;i++){
+        sum+=num1[i];
+      }
+      return sum;
+    }
+    return num1+num2;
+
   }
+
 }
 
 // export default sayHello=()=>{
--- test/appTest.js
+++ test/appTest.js
@@ -1,34 +1,43 @@
 const assert = require('chai').assert;
+const expect = require('chai').expect;
+
 //const sayHello = require('../app').sayHello;
 //const addNumbers = require('../app').addNumbers;
 const app = require('../app');
 
 // Results
 sayHelloResult = app.sayHello();
-addNumbersResult = app.addNumbers(5,5);
+addNumbersResult = app.addNumbers(5, 5);
 
-describe('App', function(){
-  describe('sayHello()', function(){
-    it('sayHello should return hello', function(){
+describe('App', function () {
+  describe('sayHello()', function () {
+    it('sayHello should return hello', function () {
       //let result = app.sayHello();
       assert.equal(sayHelloResult, 'hello');
     });
 
-    it('sayHello should return type string', function(){
+    it('sayHello should return type string', function () {
       //let result = app.sayHello();
       assert.typeOf(sayHelloResult, 'string');
     });
   });
 
-  describe('addNumbers()', function(){
-    it('addNumbers should be above 5', function(){
+  describe('addNumbers()', function () {
+    it('addNumbers should be above 5', function () {
       //let result = app.addNumbers(5,5);
       assert.isAbove(addNumbersResult, 5);
     });
 
-    it('addNumbers should return type number', function(){
+    it('addNumbers should return type number', function () {
       //let result = app.addNumbers(5,5);
       assert.typeOf(addNumbersResult, 'number');
     });
+  });
+  describe('add', function () {
+    it('result should be 4', function () {
+      expect(app.add(2, 2)).to.equal(4);
+    });
+
+
   });
 });
